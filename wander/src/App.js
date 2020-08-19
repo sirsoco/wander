3b055253085cd64ea8c94a47a678005133ba3c52
andrewwestgard <westgaar9629@uwec.edu>
2020-08-19 07:54:24 -0500

    imported header and form to app.js and renamed files to be more appropriate

--- wander/src/App.js
+++ wander/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap'
 import firebase from './config.js'
+import Header from './components/Header/Header'
+import Form from './components/userForm/Form'
 
 
 var provider = new firebase.auth.GoogleAuthProvider();
@@ -9,6 +11,12 @@ var provider = new firebase.auth.GoogleAuthProvider();
 function App() {
   return (
     <div className="App">
+    <div>
+      <Header />
+    </div>
+    <div>
+      <Form />
+    </div>
   <Button onClick={firebase.auth().signInWithRedirect(provider).then((result) => {console.log(result)})} variant="primary" size="lg" block>
     Block level button
   </Button>
--- wander/src/components/Header/Header.js
+++ wander/src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Nav.css'
+import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton';
