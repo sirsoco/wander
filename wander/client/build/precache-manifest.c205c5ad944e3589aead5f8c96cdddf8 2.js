96edba31784cf8bff0e8caa16570d5947a334651
trepear <tbuhrsmith@gmail.com>
2020-08-20 09:04:23 -0500

    pulling redirect branch onto my local device

---
+++ wander/client/build/precache-manifest.c205c5ad944e3589aead5f8c96cdddf8 2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19c7abae5bb9f96678b3ebdc08a474b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf07c5e72bac14c8e7e8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c88c6e6f8c4a14e1e5b",
+    "url": "/static/js/2.fb2fb71b.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/static/js/2.fb2fb71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf07c5e72bac14c8e7e8",
+    "url": "/static/js/main.d8bf29c1.chunk.js"
+  },
+  {
+    "revision": "3af2285d71e9fe13016f",
+    "url": "/static/js/runtime-main.90fc4d6f.js"
+  }
+]);
