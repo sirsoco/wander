f0c5196b0c79809572ef75ec3ec89cbeb9eb25cb
sam <moha0477@umn.edu>
2020-08-23 14:38:12 -0500

    idk

---
+++ wander/client/src/pages/map 2.js
@@ -0,0 +1,26 @@
+// import React, { Component } from "react";
+// // import GoogleMapReact from 'google-map-react';
+// import Map from '../components/Map/index.js';
+// // import API from "../utils/api";
+
+
+// class SimpleMap extends Component {
+
+
+//   render() {
+//     return (
+//       // Important! Always set the container height explicitly
+//       <div style={{ height: '100vh', width: '100%' }}>
+//         <Map    
+//           defaultCenter={this.props.center}
+//           center={this.state.center}
+//           defaultZoom={this.props.zoom}
+//           onChildMouseEnter={this.onChildMouseEnter}
+//           onChildMouseLeave={this.onChildMouseLeave}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default SimpleMap;
