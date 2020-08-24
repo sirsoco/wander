de2acfee64589b60a75915d19ec6bbde77ee146c
sam <moha0477@umn.edu>
2020-08-24 14:50:56 -0500

    successfully rendered profile into to the page

--- wander/client/src/pages/profile.js
+++ wander/client/src/pages/profile.js
@@ -3,23 +3,36 @@ import React, { useContext, useState, useEffect } from "react";
 // import axios from "axios";
 import UserContext from '../utils/userContext'
 import API from '../utils/api'
+import {
+  Card,
+  CardBlock,
+  CardFooter,
+  CardTitle,
+  CardText,
+} from 'react-bootstrap-card';
+import {
+  Col,
+  Row,
+} from 'react-bootstrap';
 // import "/src/components/userForm/style.css";
 // import UserContext from "../utils/userContext";
 
 // ***** Profile PAGE ******
 // =============================================================
-const [profile, setProfile] = useState({
-    name: "",
-    age: "",
-    career: "",
-    education: "",
-    currentLocation: "",
-    hobbies: "",
-    destination: "",
-    photoURL: ""
-  })
 
 function Profile(props) {
+    const [profile, setProfile] = useState({
+        name: "",
+        age: "",
+        career: "",
+        education: "",
+        currentLocation: "",
+        hobbies: "",
+        destination: "",
+        photoURL: ""
+      })
+
+
   console.log("PROFILE PROPS: ", props)
   const { matchId } = props.location.state
   useEffect(() => {
@@ -28,23 +41,52 @@ function Profile(props) {
         (response) => {
           console.log("RESPONSE: ", response);
           setProfile({
-            name: response.name,
-            age: response.age,
-            career: response.career,
-            education: response.education,
-            currentLocation: response.currentLocation,
-            hobbies: response.hobbies,
-            destination: response.destination,
-            photoURL: response.photoURL
+            name: response[0].name,
+            age: response[0].age,
+            career: response[0].career,
+            education: response[0].education,
+            currentLocation: response[0].currentLocation,
+            hobbies: response[0].hobbies,
+            destination: response[0].destination,
+            photoURL: response[0].photoURL
           })
+          console.log("name: ",response[0].name)
         })
     }
   }, [matchId]);
 
   return (
-    <h1>Hello World</h1>
+    <Card>
+      <CardBlock>
+        <CardTitle>
+        {profile.name}
+            </CardTitle>
+            <p> 
+            {profile.age}
+            </p>
+            <p>
+            {profile.career}
+            </p>
+            <p>
+            {profile.education}
+            </p>
+            <p>
+            {profile.currentLocation}
+            </p>
+            <p>
+            {profile.hobbies}
+            </p>
+            <p>
+            {profile.destination}
+            </p>
+            <img src={profile.photoURL} />   
+        </CardBlock>
+        <CardFooter className="text-xs-center">
+            Some Footer
+        </CardFooter>
+    </Card>
   );
+};
 
-}
 export default Profile
 
