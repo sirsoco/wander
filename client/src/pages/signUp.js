4f2f52778856c9db097b4b36f917018039e50098
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-09-14 02:21:45 -0500

    changed font style of signup

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,70 +1,65 @@
-import React, {useContext, useState, useEffect} from "react";
-import firebase from '../config.js';
+import React, { useContext, useState, useEffect } from "react";
+import firebase from "../config.js";
 import { Button } from "react-bootstrap";
 import axios from "axios";
 import UserContext from "../utils/userContext";
-import MinimalisticIntro from "../components/MinimalisticIntro"
-import './pageStyles/signUp.css';
+import Login from "../components/login/index.js";
+import "./pageStyles/signUp.css";
 
 // ***** LOGIN PAGE ******
 // =============================================================
 var provider = new firebase.auth.GoogleAuthProvider();
 
-function Signup(props) { 
+function Signup(props) {
   // CONNECT TO THE GLOBAL USER OBJECT to set the user's unique ID from firebase
   const userState = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(true);
-  
+
   const signUp = () => {
-    firebase.auth().signInWithPopup(provider)
-      .then(({user}) => {
-        // AXIOS CALL 
-        axios.post("/api/user", {uid: user.uid, photoURL: user.photoURL})
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        // AXIOS CALL
+        axios
+          .post("/api/user", { uid: user.uid, photoURL: user.photoURL })
           .then(() => {
             console.log(user);
             // SET THE ID IN THE GLOBAL USER STATE
-            userState.setID(user.uid)
+            userState.setID(user.uid);
             // Save refresh-token to local storage
             // userState.setImage(user.photoURL)
-            localStorage.setItem("token", user.refreshToken)
-            // save photo URL to local storage 
-            localStorage.setItem("photo", user.photoURL)
+            localStorage.setItem("token", user.refreshToken);
+            // save photo URL to local storage
+            localStorage.setItem("photo", user.photoURL);
             // Redirect to main pg (register.js)
-            props.history.push("/register")
-
-          }).catch(err => console.log("ERROR: ", err))        
+            props.history.push("/register");
+          })
+          .catch((err) => console.log("ERROR: ", err));
       })
-      .catch(console.error)
-    };
+      .catch(console.error);
+  };
 
-    useEffect(() => {
-      const token = localStorage.getItem("token");
-      if(token){
-        props.history.push("/map")
-      } else {
-        setIsLoading(false)
-      }
-    }, []);
-
-    if(isLoading){
-      return (
-        <p>Loading ...</p>
-      )
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      props.history.push("/map");
     } else {
-      return (
-        <div>
-          
-          <MinimalisticIntro>
-
-          </MinimalisticIntro>
-        
-        <Button onClick={signUp} variant="primary" size="lg" block>
-        Sign-Up With Google
-          </Button>
-        </div>
-      )
+      setIsLoading(false);
     }
+  }, []);
 
+  if (isLoading) {
+    return <p>Loading ...</p>;
+  } else {
+    return (
+      <div>
+        <Login></Login>
+        <Button onClick={signUp} variant="primary" size="lg" block>
+          Sign-Up With Google
+        </Button>
+      </div>
+    );
+  }
 }
-export default Signup
-
+export default Signup;
