9e933e94114f9e8e0d01d34371ddf169108dcc51
sirsoco <66388516+sirsoco@users.noreply.github.com>
2021-04-14 23:51:29 -0500

    fixed issue when going back to map from profile

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import useAuth from '../utils/useAuth';
 
 
-export default function LoginPage() {
+export default function LoginPage(props) {
     let history = useHistory();
     let location = useLocation();
     let auth = useAuth();
@@ -15,7 +15,21 @@ export default function LoginPage() {
             history.replace(from);
         });
     };
+
+    useEffect(() => {
+        // checking to see if user is already authenticated
+        const token = localStorage.getItem("token");
     
+        if (token) {
+          // set authentication to true
+          auth.setID(true);
+          props.history.push("/map");
+        } 
+        else {
+          
+        }
+    })
+
     return(
         <div>
             <p>You must log in to view the page at
