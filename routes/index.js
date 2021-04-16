638322921f73fc2457a7086d4969a986528fa11d
sirsoco <66388516+sirsoco@users.noreply.github.com>
2021-04-16 00:24:21 -0500

    changed node version back to 6.14.5

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.use("/api", apiRoutes);
 
 // For anything else, render the html page
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
